1f2df38ea79c0b8eaba29bb5577f2b29f9deeee1
Sayaji Laxman Chavan <sayajichavan9999@gmail.com>
2021-05-13 03:43:28 +0530

    Add files via upload

--- books/urls.py
+++ books/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='book.all'),
+    path('<int:id>', views.show, name='book.show')
 ]
