0bf77959f2c1d14a8ea80ccb2333e58b9621d785
Sayaji Laxman Chavan <sayajichavan9999@gmail.com>
2021-05-14 01:47:03 +0530

    Updated Files

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=256)
@@ -7,3 +8,12 @@ class Book(models.Model):
     thumbnailUrl = models.CharField(max_length=256, null=True)
     shortDescription = models.CharField(max_length=256, null=True)
     longDescription = models.TextField(null=True)
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
+
+
+class Review(models.Model):
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+    book_id = models.BigIntegerField(default=3)
--- books/views.py
+++ books/views.py
@@ -1,21 +1,35 @@
-from django.shortcuts import render
-import json
-
-booksData = open(r'C:\Users\SayajiChavan\Desktop\LearningDjango\bookstore-project\books.json').read()
-
-data = json.loads(booksData)
+from django.shortcuts import render, get_object_or_404, redirect
+from books.models import Book, Review
+from django.views.generic import ListView, DetailView
 
 
-def index(request):
-    context = {'books': data}
-    return render(request, "books/index.html", context)
+class BookListView(ListView):
+    def get_queryset(self):
+        return Book.objects.all()
+
+
+class BookDetailView(DetailView):
+    model = Book
+
+
+
+'''def index(request):
+    dbData = Book.objects.all()
+     context = {'books': dbData}
+    return render(request, "books/book_list.html", context)
+
+
 
 
 def show(request, id):
-    singleBook = list()
-    for book in data:
-        if book['id'] == id:
-            singleBook = book
+    singleBook = get_object_or_404(Book, pk=id)
+    reviews = Review.objects.filter(book_id=id).order_by('-created_at')
+    context = {'book': singleBook, 'reviews': reviews}
+    return render(request, 'books/book_detail.html', context)
 
-    context = {'book': singleBook}
-    return render(request, 'books/show.html', context)
+'''
+def review(request, id):
+    body = request.POST['review']
+    newReview = Review(body=body, book_id=id)
+    newReview.save()
+    return redirect('/book')
